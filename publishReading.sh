62ae1cd8855ab6b1cb1515e4721324d193778b18
greenbreakfast <lazardemin@gmail.com>
2018-02-20 14:40:51 -0500

    changed how sensor reading is passed around

--- publishReading.sh
+++ publishReading.sh
@@ -3,27 +3,30 @@
 # include the json parsing library
 . /usr/share/libubox/jshn.sh
 
-value=0
-
 # get reading from DS18B20 1-wire tempearture sensor
+#	echo returns the sensor reading
 getDS18B20Reading () {
 	# read device unique ID from list of slaves (assumes a single 1-wire slave)
 	local devId=$(cat /sys/devices/w1_bus_master1/w1_master_slaves)
 	# get the reading from the sensor
 	value=$(awk -F= '/t=/ {printf "%.03f\n", $2/1000}' /sys/devices/w1_bus_master1/$devId/w1_slave)
+
+	echo "$value"
 }
 
 # update AWS IoT Thing shadow
 #  arg1 - AWS IoT Thing Name
+#  arg2 - value
 updateThingShadow () {
 	local thingId="$1"
+	local value="$2"
 
 	# init JSON for AWS IoT device shadow update
 	json_init
 	json_add_object "state"
 	json_add_object "reported"
 
-	json_add_double "temperature" $value
+	json_add_double "temperature" "$value"
 
 	# generate json
 	jsonOutput=$(json_dump)
@@ -41,10 +44,11 @@ main () {
 	local thingId="$1"
 
 	# read DS18B20 sensor
-	getDS18B20Reading
+	reading=$(getDS18B20Reading)
+	echo "> Sensor reading: '$reading'"
 
 	# update AWS IoT Thing shadow with latest measurement
-	updateThingShadow "$thingId"
+	updateThingShadow "$thingId" "$reading"
 }
 
 ## parse arguments
